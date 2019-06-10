48a8f9a1e972944f0d34ddf9b4e8d391e0bd0d38
Vlad-Ștefan Harbuz <vlad@vladh.net>
2019-06-11 00:08:09 +0200

    Initial commit.

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'comma-dangle': 0,
+    'no-new': 0,
+    'space-before-function-paren': ['error', 'never']
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
---
+++ src/components/App.vue
@@ -0,0 +1,2 @@
+<template lang="pug" src="../templates/App.pug"></template>
+<style lang="stylus" src="../stylesheets/App.styl"></style>
