448a3bd48b5e545ff9e9e00b0a164c84b735578a
harry <harry.hunt@yaay.ch>
2019-07-05 11:58:27 +0200

    make paths lowercase everywhere kind of
    :harry-grimacing:

---
+++ src/components/app.vue
@@ -0,0 +1,8 @@
+<template lang="pug" src="../templates/app.pug"></template>
+<style lang="stylus" src="../stylesheets/app.styl"></style>
+
+<script>
+export default {
+  name: 'app',
+}
+</script>
---
+++ src/components/home.vue
@@ -0,0 +1,15 @@
+<template lang="pug" src="../templates/home.pug"></template>
+<style lang="stylus" src="../stylesheets/home.styl"></style>
+
+<script>
+import Hero from './hero.vue'
+import IgFeed from './ig_feed.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Hero,
+    IgFeed,
+  }
+}
+</script>
