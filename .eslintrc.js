a429361c9ad589a8432e57aeb15a71fbd3c56708
Vlad-Ștefan Harbuz <vlad@vladh.net>
2019-11-04 19:01:51 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,19 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    es6: true,
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
+  extends: [
+    'eslint:recommended',
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'comma-dangle': 0,
     'no-new': 0,
-    'space-before-function-paren': ['error', 'never']
+    'space-before-function-paren': ['error', 'never'],
+    'no-unused-vars': ['error', {'args': 'none'}],
   },
   parserOptions: {
     parser: 'babel-eslint'
